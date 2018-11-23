333f28800cb91bc6e61cfed639c55b4507a34e42
Michael Beckemeyer <m.beckemeyer@gmx.net>
2018-11-23 11:33:22 +0100

    Expose deferred<T>

---
+++ include/prequel/deferred.hpp
@@ -0,0 +1,13 @@
+#ifndef PREQUEL_DEFERRED_HPP
+#define PREQUEL_DEFERRED_HPP
+
+#include <prequel/detail/deferred.hpp>
+
+namespace prequel {
+
+// FIXME move the class here.
+using detail::deferred;
+
+}
+
+#endif // PREQUEL_DEFERRED_HPP
